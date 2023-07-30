65430ac92c2b876cd0cf0f8814fbd48f7fa8048c
samiahmadi <samiahmadi@localhost>
2023-07-29 22:47:59 -0400

    New Class

--- src/DemoDay2/DemoDay2.java
+++ src/DemoDay2/DemoDay2.java
@@ -2,9 +2,13 @@ package DemoDay2;
 
 public class DemoDay2 {
 	
+	String name = "Ahmadi";
+	String county = "Afghanistan";
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Test");
+		System.out.println("John Cena");
 	}
 		
 	public static void add (int a, int b) {
@@ -23,4 +27,20 @@ public class DemoDay2 {
 	public static void add (int a, int b, int c) {
 		
 		System.out.println(a + b + c);
-}}
+}
+	public static void getInfo () {
+		int a = 10;
+		int b = 200;
+		
+		System.out.println(a);
+		System.out.println(b);
+	
+}
+	public static void Subtract () {
+		int Sub1 = 200;
+		int Sub2 = 500;
+		
+		System.out.println(Sub1);
+		System.out.println(Sub2);
+}
+^
